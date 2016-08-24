dc6f0b105edc76bde4bd418962abd6dcd1631489
Cameron Behar <0x24a537r9@gmail.com>
2016-08-23 18:49:28 -0700

    Moved constants to command line arguments, and added a bunch of features along those lines.

--- geofence_monitor.py
+++ geofence_monitor.py
@@ -1,6 +1,8 @@
 import argparse
+import flask
 import json
 import logging
+import re
 import requests
 import smtplib
 import threading
@@ -10,23 +12,130 @@ import urllib2
 
 from email.mime.text import MIMEText
 from shapely.geometry import shape
-from flask import Flask
-
-parser = argparse.ArgumentParser(
-    description='Monitors cars, triggering an email alert if any leave their prescribed geofences.')
-parser.add_argument('--mailgun_api_key', dest='mailgun_api_key',
-                    default='key-db805e58c7522624b6b6c7fbb96dcbb0',
-                    help='The API key for the mailgun account used to send alert emails')
-args = parser.parse_args()
 
 
-ALERT_EMAILS = ['Cameron Behar <0x24a537r9@gmail.com>']
-MONITOR_EMAIL = 'Geofence monitor <engineering+geofence_monitor@skurt.com>'
+app = flask.Flask(__name__)
 
-CAR_IDS = set(range(1, 12))
-CAR_STATUS_ENDPOINT = 'http://skurt-interview-api.herokuapp.com/carStatus/%s'
 
-app = Flask(__name__)
+def parse_ids(arg):
+  match = re.match(r'^(\d+)-(\d+)$', arg)
+  if match:
+    return range(int(match.group(1)), int(match.group(2)) + 1)
+  elif re.match(r'^\d+', arg):
+    return [int(arg)]
+  raise ValueError('Invalid id argument: "%s"' % arg)
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Monitors cars, triggering an email alert if any '
+                                               'leave their prescribed geofences.')
+  arg_defs = [{
+    'name': 'car_ids',
+    'type': parse_ids,
+    'nargs': '+',
+    'help': 'The car IDs to monitor. IDs can be specified as single IDs or ID ranges such as "2-8"',
+  }, {
+    'name': '--car_status_endpoint',
+    'dest': 'car_status_endpoint',
+    'default': 'http://skurt-interview-api.herokuapp.com/carStatus/%s',
+    'help': 'The URL pattern for the car status endpoint, with "%%s" to indicate the id insertion '
+            'point',
+  }, {
+    'name': '--alert_emails',
+    'dest': 'alert_emails',
+    'default': ['Cameron Behar <0x24a537r9@gmail.com>'],
+    'type': lambda s: re.split(r'\s*,\s*', s),
+    'help': 'The email addresses to alert in case of a car out of its geofence',
+  }, {
+    'name': '--monitor_email',
+    'dest': 'monitor_email',
+    'default': 'Geofence monitor <engineering+geofence_monitor@skurt.com>',
+    'help': 'The email addresses from which to send alerts',
+  }, {
+    'name': '--poll_period_s',
+    'dest': 'poll_period_s',
+    'default': 5 * 60,
+    'type': int,
+    'help': 'The period (in seconds) with which to poll the server for car statuses',
+  }, {
+    'name': '--max_poll_qps',
+    'dest': 'poll_query_delay_s',
+    'default': 1,
+    'type': lambda arg: 1 / float(arg),
+    'help': 'The maximum QPS with which to poll the server for car statuses',
+  }, {
+    'name': '--mailgun_messages_endpoint',
+    'dest': 'mailgun_messages_endpoint',
+    'default': 'https://api.mailgun.net/v3/sandboxf3f15ea9e4c743199c24cb3b628208c0.mailgun.org/'
+               'messages',
+    'help': 'The URL for the Mailgun messages endpoint',
+  }, {
+    'name': '--mailgun_api_key',
+    'dest': 'mailgun_api_key',
+    'default': 'key-db805e58c7522624b6b6c7fbb96dcbb0',
+    'help': 'The API key for the mailgun account used to send alert emails',
+  }, {
+    'name': '--log',
+    'dest': 'logging_level',
+    'default': logging.INFO,
+    'type': lambda level: getattr(logging, level),
+    'choices': (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL),
+    'help': 'The logging level to use',
+  }]
+  for arg_def in arg_defs:
+    parser.add_argument(arg_def.pop('name'), **arg_def)
+  args = parser.parse_args()
+
+  # Flatten the car_ids args into a set.
+  args.car_ids = reduce(lambda acc, ids: acc | set(ids), args.car_ids, set())
+  return args
+
+
+def poll():
+  # Find the set of out-of-bounds cars.
+  out_of_bounds_car_ids = set()
+  for car_id in args.car_ids:
+    start_time = time.time()
+    geojson = json.load(urllib2.urlopen(args.car_status_endpoint % car_id))
+
+    # Extract the first Point feature in the GeoJSON response as the car's coordinates.
+    car = next((feature for feature in geojson['features']
+                if feature['geometry']['type'] == 'Point'), None)
+    if not car:
+      logging.warning('No car coordinates for car %s in status response: %s', car_id, geojson)
+      continue
+
+    # Extract all Polygon features as the car's geofences.
+    geofences = [feature for feature in geojson['features']
+                 if feature['geometry']['type'] == 'Polygon']
+
+    # Mark the car as out-of-bounds, if necessary.
+    if not any(shape(geofence['geometry']).contains(shape(car['geometry']))
+               for geofence in geofences):
+      logging.info('Car found outside of geofence: %s', car['properties']['id'])
+      out_of_bounds_car_ids.add(car_id)
+
+    # Throttle, if necessary.
+    throttle_delay = args.poll_query_delay_s - (time.time() - start_time)
+    if throttle_delay > 0:
+      logging.debug('Throttling for %s seconds.' % throttle_delay)
+      time.sleep(throttle_delay)
+
+  # Alert by email if necessary.
+  if out_of_bounds_car_ids:
+    requests.post(
+        args.mailgun_messages_endpoint,
+        auth=('api', args.mailgun_api_key),
+        data={
+          'from': args.monitor_email,
+          'to': ', '.join(args.alert_emails),
+          'subject': '[ALERT] Cars outside of geofences',
+          'text': 'Cars [%s] are outside of their geofences!' %
+                  ', '.join(str(car_id) for car_id in sorted(out_of_bounds_car_ids))
+        })
+  
+  # Queue another poll event.
+  threading.Timer(args.poll_period_s, poll).start()
 
 
 @app.route('/')
@@ -39,43 +148,10 @@ def ok():
   return 'ok'
 
 
-def poll():
-  # Find the set of out-of-bounds cars.
-  out_of_bounds_car_ids = set()
-  for car_id in CAR_IDS:
-    geojson = json.load(urllib2.urlopen(CAR_STATUS_ENDPOINT % car_id))
-
-    car = next((feature for feature in geojson['features']
-                if feature['geometry']['type'] == 'Point'), None)
-    if not car:
-      logging.warning('No car coordinates in %s', geojson)
-      continue
-
-    geofences = [feature for feature in geojson['features']
-                 if feature['geometry']['type'] == 'Polygon']
-
-    if not any(shape(geofence['geometry']).contains(shape(car['geometry']))
-               for geofence in geofences):
-      print 'Out of bounds: %s' % car['properties']['id']
-      out_of_bounds_car_ids.add(car_id)
-
-  # Alert if necessary.
-  if out_of_bounds_car_ids:
-    requests.post(
-        'https://api.mailgun.net/v3/sandboxf3f15ea9e4c743199c24cb3b628208c0.mailgun.org/messages',
-        auth=('api', args.mailgun_api_key),
-        data={
-            'from': MONITOR_EMAIL,
-            'to': ', '.join(ALERT_EMAILS),
-            'subject': '[Alert] Cars outside of geofences',
-            'text': 'Cars [%s] are out of bounds!' %
-                    ', '.join(str(car_id) for car_id in sorted(out_of_bounds_car_ids))
-        })
-  
-  # Queue another poll event.
-  threading.Timer(10, poll).start()
-
-
 if __name__ == '__main__':
+  args = parse_args()
+  logging.basicConfig(level=args.logging_level,
+                      format='%(levelname)s %(asctime)s [%(name)s]: %(message)s')
+
   poll()
   app.run()
